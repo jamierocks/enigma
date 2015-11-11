7827b68e1ec0c0c4e44d78af76a0c35cb0a17c9e
Jamie Mansfield <malgm@lapis.blue>
2015-11-11 16:13:14 +0000

    Fix method reading

--- src/main/java/cuchaz/enigma/mapping/MappingsReader.java
+++ src/main/java/cuchaz/enigma/mapping/MappingsReader.java
@@ -45,39 +45,18 @@ public class MappingsReader {
 				//mappings.getClassByDeobf(deobfClass).addFieldMapping(new FieldMapping());
 				// TODO:
 			} else if (line.startsWith("MD: ")) {
+				String[] obfSplit = split[0].split("/");
+
 				String[] deobfSplit = split[2].split("/");
 				String deobfClass = split[2]
 						.substring(0, split[2].length() - (deobfSplit[deobfSplit.length - 1].length() + 1));
+
 				mappings.getClassByDeobf(deobfClass)
-						.addMethodMapping(new MethodMapping(split[1], new Signature(split[2]), split[3]));
+						.addMethodMapping(new MethodMapping(obfSplit[obfSplit.length - 1],
+								new Signature(split[1]), deobfSplit[deobfSplit.length - 1]));
 			}
 		}
 
 		return mappings;
-	}
-
-	private ArgumentMapping readArgument(String[] parts) {
-		return new ArgumentMapping(Integer.parseInt(parts[1]), parts[2]);
-	}
-
-	private ClassMapping readClass(String[] parts, boolean makeSimple) {
-		if (parts.length == 2) {
-			return new ClassMapping(parts[1]);
-		} else {
-			return new ClassMapping(parts[1], parts[2]);
-		}
-	}
-
-	/* TEMP */
-	protected FieldMapping readField(String[] parts) {
-		return new FieldMapping(parts[1], new Type(parts[3]), parts[2]);
-	}
-
-	private MethodMapping readMethod(String[] parts) {
-		if (parts.length == 3) {
-			return new MethodMapping(parts[1], new Signature(parts[2]));
-		} else {
-			return new MethodMapping(parts[1], new Signature(parts[3]), parts[2]);
-		}
 	}
 }
